e123a30974ac22d9c4c6701a4292e368db3f8c47
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-02-24 02:23:57 +0530

    Add Framer Motion and search input to Navbar component

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -7,6 +7,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import Drawer from "./Drawer";
 import { FaUser } from "react-icons/fa6";
 import Link from 'next/link';
+import { PlaceholdersAndVanishInput } from '../ui/placeholders-and-vanish-input';
 
 
 const Navbar = () => {
@@ -27,15 +28,41 @@ const Navbar = () => {
         updateActiveLinks(pathname)
     }, [pathname])
 
+    const placeholders = [
+        "What's the first rule of Fight Club?",
+        "Who is Tyler Durden?",
+        "Where is Andrew Laeddis Hiding?",
+        "Write a Javascript method to reverse a string",
+        "How to assemble your own PC?",
+    ];
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    };
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submitted");
+    };
+
     return (
         <nav className="lg:h-[86px] md:h-[70px] h-[60px] min-w-full lg:pt-[10px] px-[5px] shadow-xl shadow-black/10 lg:bg-white bg-[#E7E7E7] sticky top-0 left-0 z-50">
-            <div className="h-full w-full">
-                <div className=" float-left flex ml-[4%] h-full">
+            <div className="h-full w-full flex justify-between items-center">
+                <div className=" flex ml-[4%] h-full">
                     <a href={"/"}><img className="xl:w-[154px] md:w-[120px] w-[80px] xl:h-[74px] lg:h-[60px] md:h-[60px] h-[50px] lg:pb-[10px] lg:pt-0 pt-[12px]" src={"./logo/md-logo.png"} alt="muscledenz" /></a>
                 </div>
 
+                <div className='w-full'>
+                    <PlaceholdersAndVanishInput
+                        placeholders={placeholders}
+                        onChange={handleChange}
+                        onSubmit={onSubmit}
+
+                    />
+                </div>
+
                 {/* NavLinks For Desktop */}
-                <div className="float-right h-full justify-end gap-10 mr-10  items-center lg:flex hidden">
+                <div className="h-full justify-end gap-10 mr-10  items-center lg:flex hidden">
+           
                     {
                         NavLinks && NavLinks.map((link, index) => (
                             <Link
