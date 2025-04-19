1ccb7e73f054e52a8a4c6ef8334c28f82945b0bb
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-04-19 11:39:07 +0530

    Update viewport settings in layout: enable maximum and minimum scale for better responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,8 +52,8 @@ export const metadata: Metadata = {
   viewport: {
     width: "device-width",
     initialScale: 1,
-    // maximumScale: 1,
-    // minimumScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
     userScalable: false,
   },
   robots: {
