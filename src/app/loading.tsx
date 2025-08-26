6c5201721cdea2a9a8091ebc0806ea3744246dde
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-08-26 23:27:27 +0530

    feat(Product): implement infinite scrolling for product listings and enhance loading state
    - Added react-infinite-scroll-component for improved product loading experience.
    - Refactored ProductListingPage to fetch products dynamically using useInfiniteQuery.
    - Updated loading state handling to provide user feedback during data fetching.
    - Adjusted product rendering logic to accommodate new data structure and loading states.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,9 @@
+import clsx from 'clsx';
 import React from 'react';
 
-const Loading = () => {
+const Loading = ({height}:{height?:string}) => {
   return (
-    <div className="flex justify-center items-center w-full h-[90vh]">
+    <div className={clsx("flex justify-center items-center w-full ", height ? height :"h-[90vh]")}>
       <div className="flex flex-col items-center space-y-2">
         <div className="w-8 h-8 border-4 border-[#008ED6] border-t-transparent rounded-full animate-spin" />
         <p className="text-sm text-gray-500">Hang tight, loading...</p>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,61 @@
 "use client";
-import ProductGrid from "@/components/Product/product-grid";
 import { ProductSorter } from "@/components/Product/product-sorter";
+import ProductCard from "@/components/Product/ProductCard";
 import ProductNotFound from "@/components/ProductNotFound";
 import { categories } from "@/config/categories.config";
-import { products } from "@/config/products.config";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import strapi from "@/sdk";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loading from "../loading";
 
 export default function ProductListingPage() {
   const searchParams = useSearchParams();
   const category = searchParams.get("category");
   const query = searchParams.get("query");
-  const [fProducts, setFProducts] = useState(products);
+  //const [fProducts, setFProducts] = useState(products);
+  const router = useRouter()
+  const q = searchParams.get("q") || ""
+
+  const fetchProducts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+    const res = await strapi.find("products", {
+      pagination: {
+        page: pageParam,
+        pageSize: 5,
+      },
+      populate: ["thumbnail"]
+    }) as any
+ 
+    return {
+      data: res.data,
+      nextPage:
+        pageParam < res.meta.pagination.pageCount ? pageParam + 1 : undefined,
+    };
+  }
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+    isLoading,
+  } = useInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: fetchProducts,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+    initialPageParam: 1,
+  });
+
+ 
+
+  const products = data?.pages.flatMap((page) => page.data) ?? [];
+
 
   useEffect(() => {
     let filtered = products;
-
     if (category && category !== "all") {
       filtered = filtered.filter((p) => p.category.slug === category);
     }
@@ -28,9 +68,9 @@ export default function ProductListingPage() {
       );
     }
 
-    setFProducts(filtered);
+ 
   }, [category, query]);
-  if (!fProducts.length) {
+  if (!products.length && !isLoading)  {
     return <ProductNotFound />;
   }
   return (
@@ -49,12 +89,46 @@ export default function ProductListingPage() {
         <div className="flex-1  px-2 md:px-4">
           <div className="flex  flex-wrap justify-between items-center mb-6">
             <p className="text-muted-foreground my-4 ">
-              {fProducts.length} products
+              {products.length} products
             </p>
             <ProductSorter />
           </div>
+          {/* 
+          <ProductGrid products={fProducts} /> */}
 
-          <ProductGrid products={fProducts} />
+          <div className="flex-1">
+            <div className="flex-1">
+              {
+                isLoading ? <div className=" flex justify-center items-center"><Loading  /></div> : <>
+                  <InfiniteScroll
+                    dataLength={products.length}
+                    next={fetchNextPage}
+                    hasMore={!!hasNextPage}
+                    loader={<div className="flex justify-center w-full items-center mt-4"><Loading height="20px" /></div>}
+                  >
+                    {products.length > 0 ? (
+                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+                        {products.map((product) => (
+                          <motion.div
+                            key={product.id}
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.3, delay: Number(product.id) * 0.05 }}
+                            whileHover={{ y: -5 }}
+                            className="h-full"
+                          >
+                            <ProductCard product={product} />
+                          </motion.div>
+                        ))}
+                      </div>
+                    ) : (
+                      <ProductNotFound />
+                    )}
+                  </InfiniteScroll>
+                </>
+              }
+            </div>
+          </div>
         </div>
       </div>
     </div>
