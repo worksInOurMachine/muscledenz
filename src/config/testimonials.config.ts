79de842a700f7b86eca4822b084de365a80d35d7
Ajay Dhakad <144946727+Ajay-Dhakad@users.noreply.github.com>
2025-11-14 13:11:25 +0530

    Clear testimonials array in testimonials.config.ts
    Removed existing testimonials and replaced with an empty array.

--- src/config/testimonials.config.ts
+++ src/config/testimonials.config.ts
@@ -1,17 +1,3 @@
 import { TestimonialType } from "@/types/testimonials";
 
-export const testimonials: TestimonialType[] = [
-    {
-      name: "Gautam Hasija",
-      avatarUrl: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-CmmqnkUJpoHFztm77lfwPNl91Xc3fL.png",
-      stars: 5,
-      description: "I've tried a lot of online supplement stores, but Muscledenz stands out for its high-quality products and excellent service."
-    },
-    {
-      name: "Ananya Sharma",
-      avatarUrl: "https://randomuser.me/api/portraits/women/44.jpg",
-      stars: 5,
-      description: "Muscledenz supplements have significantly improved my endurance and strength. Highly recommended for serious athletes!"
-    },
-   
-  ]
+export const testimonials: any = []
